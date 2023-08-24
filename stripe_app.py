11b7dab72b65fe54cd4ab02c9486c0eebc342e60
Michael <michael@xorgonix.com>
2023-08-24 16:55:29 -0400

    aider: Updated stripe_app.py to have all the stripe logic placed in the routes.
    # Aider chat conversation:
    
    USER: update stripe_app.py to have all the stripe logic placed in the routes
    
    ASSISTANT: To integrate Stripe functionality into your Flask application, you'll need to modify the `stripe_app.py` file.
    
    Here's a step-by-step guide on how to do it:
    
    1. Import the necessary Stripe library at the top of your file.
    2. Set your Stripe secret key.
    3. Update the `show_subscription_choices` route to fetch the subscription plans from Stripe and pass them to the template.
    4. Update the `customer_portal` route to create a Stripe Customer Portal session and redirect the user to it.
    5. Update the `confirmation` route to create a Stripe Checkout session for the selected subscription plan and redirect the user to it.
    
    Here are the changes you need to make:
    
    ```python
    stripe_app.py
    <<<<<<< HEAD
    from flask import Flask, render_template, request, redirect, url_for
    import stripe
    
    app = Flask(__name__)
    
    @app.route('/')
    def home():
        return render_template('home.html')
    
    @app.route('/customer-portal', methods=['GET', 'POST'])
    def customer_portal():
        if request.method == 'POST':
            # Handle the form submission for accessing the customer portal
            pass
        return render_template('customer_portal.html')
    
    @app.route('/show-subscription-choices', methods=['GET', 'POST'])
    def show_subscription_choices():
        if request.method == 'POST':
            # Handle the form submission for selecting a subscription plan
            pass
        return render_template('show_subscription_choices.html')
    
    @app.route('/confirmation', methods=['GET', 'POST'])
    def confirmation():
        if request.method == 'POST':
            # Handle the form submission for confirming the subscription
            pass
        return render_template('confirmation.html')
    
    if __name__ == '__main__':
        app.run(debug=True)
    =======
    from flask import Flask, render_template, request, redirect, url_for, session
    import stripe
    
    stripe.api_key = 'your-stripe-secret-key'
    
    app = Flask(__name__)
    
    @app.route('/')
    def home():
        return render_template('home.html')
    
    @app.route('/customer-portal', methods=['GET', 'POST'])
    def customer_portal():
        if 'customer_id' not in session:
            return redirect(url_for('home'))
    
        session = stripe.billing_portal.Session.create(
            customer=session['customer_id'],
            return_url=url_for('home', _external=True)
        )
        return redirect(session.url)
    
    @app.route('/show-subscription-choices', methods=['GET', 'POST'])
    def show_subscription_choices():
        plans = stripe.Plan.list()
        return render_template('show_subscription_choices.html', plans=plans)
    
    @app.route('/confirmation', methods=['GET', 'POST'])
    def confirmation():
        if 'plan_id' not in session:
            return redirect(url_for('show_subscription_choices'))
    
        checkout_session = stripe.checkout.Session.create(
            payment_method_types=['card'],
            line_items=[{
                'price': session['plan_id'],
                'quantity': 1,
            }],
            mode='subscription',
            success_url=url_for('home', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
            cancel_url=url_for('show_subscription_choices', _external=True),
        )
        return redirect(checkout_session.url)
    
    if __name__ == '__main__':
        app.run(debug=True)
    >>>>>>> updated
    ```
    
    Please replace `'your-stripe-secret-key'` with your actual Stripe secret key.
    
    Also, note that this code assumes that you are storing the customer's ID and the selected plan's ID in the Flask session. You'll need to adjust this according to how you are handling sessions in your application.
    
    This code also assumes that you have a Stripe Plan for each of your subscription choices. If you are using Prices instead of Plans, you'll need to adjust the code accordingly.

--- stripe_app.py
+++ stripe_app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import stripe
+
+stripe.api_key = 'your-stripe-secret-key'
 
 app = Flask(__name__)
 
@@ -9,24 +11,36 @@ def home():
 
 @app.route('/customer-portal', methods=['GET', 'POST'])
 def customer_portal():
-    if request.method == 'POST':
-        # Handle the form submission for accessing the customer portal
-        pass
-    return render_template('customer_portal.html')
+    if 'customer_id' not in session:
+        return redirect(url_for('home'))
+
+    session = stripe.billing_portal.Session.create(
+        customer=session['customer_id'],
+        return_url=url_for('home', _external=True)
+    )
+    return redirect(session.url)
 
 @app.route('/show-subscription-choices', methods=['GET', 'POST'])
 def show_subscription_choices():
-    if request.method == 'POST':
-        # Handle the form submission for selecting a subscription plan
-        pass
-    return render_template('show_subscription_choices.html')
+    plans = stripe.Plan.list()
+    return render_template('show_subscription_choices.html', plans=plans)
 
 @app.route('/confirmation', methods=['GET', 'POST'])
 def confirmation():
-    if request.method == 'POST':
-        # Handle the form submission for confirming the subscription
-        pass
-    return render_template('confirmation.html')
+    if 'plan_id' not in session:
+        return redirect(url_for('show_subscription_choices'))
+
+    checkout_session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price': session['plan_id'],
+            'quantity': 1,
+        }],
+        mode='subscription',
+        success_url=url_for('home', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url=url_for('show_subscription_choices', _external=True),
+    )
+    return redirect(checkout_session.url)
 
 if __name__ == '__main__':
     app.run(debug=True)
